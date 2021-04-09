80711c8e3678fb5ba0c870fb480fadf5d970417e
Alex Titov <31844606+al-dann@users.noreply.github.com>
2021-04-09 20:08:31 +0100

    admin bash scripts

---
+++ admin/admin-var.sh
@@ -0,0 +1,12 @@
+export GCP_PRJ_ID_ADM="ishim-adm"
+export GCP_PRJ_ID_DEV="ishim-dev"
+export GCP_PRJ_ID_TST="ishim-tst"
+export GCP_PRJ_ID_PRD="ishim-prd"
+
+export GCP_TF_BUCKET_ADM="gs://ishim-adm-tf-adm"
+export GCP_TF_BUCKET_DEV="gs://ishim-adm-tf-dev"
+export GCP_TF_BUCKET_TST="gs://ishim-adm-tf-tst"
+export GCP_TF_BUCKET_PRD="gs://ishim-adm-tf-prd"
+
+export GCP_TF_REGION="europe-west2"
+
