3f578d4179d33ecbb3f9f3df3d9580d2da3020ca
Alex Titov <31844606+al-dann@users.noreply.github.com>
2021-04-14 20:17:32 +0100

    use vpc

--- infrastructure/projects/ishim-dev/main.tf
+++ infrastructure/projects/ishim-dev/main.tf
@@ -1,16 +1,17 @@
 module "vpc" {
-  source  = "github.com/terraform-google-modules/terraform-google-network/modules/subnets"
-  # version = "~> 3.2.1"
+  source  = "github.com/terraform-google-modules/terraform-google-network/modules/vpc"
+  version = "~> 3.2.1"
 
   # Give the network a name and project
   project_id   = var.gcp_project_id
   network_name = "ishim-dev-vpc"
+  description  = "test network description"
 
-  subnets = [
-    {
-      subnet_name   = "ishim-dev--sn-ew2"
-      subnet_ip     = "10.154.16.0/24"
-      subnet_region = "europe-west2"
-    }
-  ]
+  # subnets = [
+  #   {
+  #     subnet_name   = "ishim-dev-sn-ew2"
+  #     subnet_ip     = "10.154.16.0/24"
+  #     subnet_region = "europe-west2"
+  #   }
+  # ]
 }
