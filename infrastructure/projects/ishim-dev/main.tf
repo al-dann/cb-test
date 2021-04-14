647f9ae359dcfe33ebd08e88afc6df5db1a6abfe
Alex Titov <31844606+al-dann@users.noreply.github.com>
2021-04-14 20:07:50 +0100

    modified source

--- infrastructure/projects/ishim-dev/main.tf
+++ infrastructure/projects/ishim-dev/main.tf
@@ -1,11 +1,11 @@
 module "vpc" {
-  source  = "github.com/terraform-google-modules/terraform-google-network/modules/vpc"
-  # https://github.com/terraform-google-modules/terraform-google-network/modules/vpc
-  # version = "~> 3.2.1"
+  source  = "github.com/terraform-google-modules/terraform-google-network/modules/subnets"
+  version = "~> 3.2.1"
 
   # Give the network a name and project
   project_id   = var.gcp_project_id
   network_name = "ishim-dev-vpc"
+  description  = "A basic network for developmen tand test purposes"
 
   subnets = [
     {
