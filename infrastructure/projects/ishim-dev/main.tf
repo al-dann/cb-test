fe19b187105cc9752d76570c4ed6a07878b83f91
Alex Titov <31844606+al-dann@users.noreply.github.com>
2021-04-14 20:00:24 +0100

    modified source path

--- infrastructure/projects/ishim-dev/main.tf
+++ infrastructure/projects/ishim-dev/main.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-  source  = "github.com/terraform-google-modules/network/google//modules/vpc?ref=v3.2.1"
+  source  = "github.com/terraform-google-modules/terraform-google-network/modules/vpc?ref=v3.2.1"
+  # https://github.com/terraform-google-modules/terraform-google-network/modules/vpc
   # version = "~> 3.2.1"
 
   # Give the network a name and project
